68bcc3fed1a9277b9b925e3df8bbe9b6829e164c
Yasha Feferholtz <yashafeferholtz@Yashas-MacBook-Pro.local>
2020-01-27 16:02:36 -0300

    New pdf graph and update script analysis

--- scripts/gef_analysis.R
+++ scripts/gef_analysis.R
@@ -1,9 +1,8 @@
 ###CBD-GEF Analysis###
 #load packages
-library(ggplot2)
-library(dplyr)
+
+
 library(broom)
-library(stringr)
 
 library(devtools)
 P <- rprojroot::find_rstudio_root_file
@@ -19,11 +18,14 @@ gef_data$Cofinancing <- gsub("$", "", gef_data$Cofinancing, fixed=TRUE)
 gef_data$Cofinancing <- gsub(",", "", gef_data$Cofinancing, fixed=TRUE)
 gef_data$Cofinancing <- as.numeric(gef_data$Cofinancing)
 
+# create new variable called sum_grant
+library(dplyr)
 gef_data <- gef_data %>%
   group_by(Countries) %>%
   mutate(sum_grant = sum(Grant))
 
 #Funding for biodiversity
+library(stringr)
 biodiv_data <- gef_data %>%
   group_by(Countries)%>%
   filter(str_detect(Focal.Areas,"Biodiversity"))
@@ -32,6 +34,7 @@ biodiv_data <- biodiv_data %>%
   mutate(sum_biodiv=sum(Grant))
 
 #plot biodiversity funding
+library(ggplot2)
 ggplot(biodiv_data) +
   geom_histogram(aes(sum_biodiv/1000000),color="black", fill="lightgray", bins=15)+
   scale_x_log10()+
@@ -49,12 +52,14 @@ biodiv_data$Period <- as.numeric(biodiv_data$Period)
 afg <- biodiv_data %>%
   filter(Countries == "Afghanistan") #filtering for a specific country
 
-ggplot(afg,aes(x=Period, y=Grant/1000000))+ #rescale depending on magnitude
+pdf(P('outputs/funding_per_period.pdf'))
+ggplot(afg, aes(x=Period, y=Grant/1000000))+ #rescale depending on magnitude
   geom_bar(stat="identity", fill="navy")+
   labs(title="Funding in each Project Period - Afghanistan",
        x="GEF Period",
        y="Grant amount (millions USD)")+
   theme_classic()
+dev.off()
 
 #Plot Funding for all countries by period
 ggplot(biodiv_data,aes(x=Period, y=Grant/1000000000))+
