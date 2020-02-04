f6c6502fb9c61be5fd9e36cae1ab2f842e405c3a
emwise19 <you@example.com>
2020-02-04 11:44:09 -0700

    waldron data output

--- scripts/Waldron2017.R
+++ scripts/Waldron2017.R
@@ -6,7 +6,4 @@ P <- rprojroot::find_rstudio_root_file
 #Read in text file
 data<-read.delim("data/Waldron_data.txt", sep="\t")
 
-library(xlsx)
-
-write.xlsx2(data, "Waldron2017Data", sheetName = "Sheet1",
-            col.names = TRUE, row.names = TRUE, append = FALSE)
+write.csv(data, P("data/Waldron.csv"), append = FALSE)
