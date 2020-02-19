903f6b588f4f4c68ef58c7191cc3559106145466
emwise19 <you@example.com>
2020-02-19 14:20:37 -0700

    Through table 4

--- scripts/RishmanDo.R
+++ scripts/RishmanDo.R
@@ -16,5 +16,41 @@ SCBD_summ<-summary(SCBD)
 #   **Table 2**
 #   
 FinNeeds <- read_xls("data/financial_needs_data3.xls")
-
+#summary stats for reported needs
 FinNeeds<-FinNeeds[order(FinNeeds$type, FinNeeds$countries),]
+
+library(dplyr)
+FinNeedsSum<-FinNeeds %>% 
+  summarise_at("newneeds", list(mean, min, max), na.rm = TRUE) 
+rownames(FinNeedsSum) = "newneeds"
+colnames(FinNeedsSum)<-c("mean","min","max")
+
+
+# **Table 3**
+# correlation table
+library(stats)
+dataforCor<-FinNeeds %>% 
+  select_if(is.numeric, toupper)
+cortbl <- cor(dataforCor, use = "complete.obs")
+
+# ** Table 4 ***
+## regression based on Anthony's data
+modelAnthony <- lm(lndomexp_thousand ~ birdspeciesthreatened +
+                   mammalspeciesthreatened +
+                   ln_landarea +
+                   governmenteffectivenessestimate +
+                   politicalstabilityandabsenceofvi +
+                   Price_Index_yr2011 +
+                   terrestrialandmarineprotectedare +
+                   lnGDP+
+                    GDP_sq, data = FinNeeds, na.action = na.exclude)
+tab<-summary(modelAnthony)
+# r-squared 65.6
+# stat significance only on landarea, bird species, and government effectiveness
+
+#checks on the model
+model1AIC<-AIC(modelAnthony, k = 2)
+model1BIC <- BIC(modelAnthony)
+
+library(car)
+model1VIF<-vif(modelAnthony) #the VIF for lnGDP and GDP_sq are VERYYY high. Problematic
