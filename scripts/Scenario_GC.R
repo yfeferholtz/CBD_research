9d585ba765f66d9f55db0d6b0e33619aa449ad7c
brking805 <60359017+brking805@users.noreply.github.com>
2020-03-09 12:15:19 -0600

    gc

--- scripts/Scenario_GC.R
+++ scripts/Scenario_GC.R
@@ -1,9 +1,9 @@
 #Global Conservation Scenario
 
 ###Assumptions###
-  # restore habitat -> 30% reduction in ag land
-  # ambitious carbon mitigation -> 30% reduction in CO2 emissions
-  # NO change from 2018 levels
+  # 30% reduction in ag land
+  # 30% reduction in CO2 emissions
+  # No GDP change from 2018 levels
 
 #setup
 rm(list=ls())
@@ -11,7 +11,7 @@ library(devtools)
 P <- rprojroot::find_rstudio_root_file
 
 #read data 
-ScenData <- read.csv(P("data/scenario_data.csv"))
+BAUData<- read.csv(P("outputs/BAUdata.csv"))
 
 
 
